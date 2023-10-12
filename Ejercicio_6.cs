181e984740f30a54373334e0282960180375be18
Hanniel-MC <147262206+Hanniel-MC@users.noreply.github.com>
2023-10-12 15:32:02 -0600

    Update Ejercicio_6.cs
    entrega de trabajo

--- Ejercicio_6.cs
+++ Ejercicio_6.cs
@@ -1,5 +1,21 @@
-/* Ejercicio 6: Pare e Impar
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese un número entero: ");
+        int numero = int.Parse(Console.ReadLine());
 
-Escribe un programa que diga si un número
-es par o impar.*/
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El número es par");
+        }
+        else
+        {
+            Console.WriteLine("El número es impar");
+        }
+
+        Console.ReadLine();
+    }
+}
