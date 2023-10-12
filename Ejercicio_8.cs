c36864a265d85c4b85252e78d918d91e2cdadb42
Hanniel-MC <147262206+Hanniel-MC@users.noreply.github.com>
2023-10-12 15:34:40 -0600

    Update Ejercicio_8.cs
    entrega de trabajo

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -1,22 +1,19 @@
-/* Ejercicio 8: Secuencia de Patrones
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
 
-Desarrolla un programa que genere y muestre
-la siguiente secuencia de patrones:
+        Console.ReadLine();
+    }
+}
 
-
-*
-
-
-**
-
-
-***
-
-
-****
-
-
-*****
-
-*/
